3972258777c0262a1f566730fcb29c9b60268dd1
alreadysons <alreadysons@gmail.com>
2023-04-05 19:33:53 +0900

    harshad number

--- algorithm.py
+++ algorithm.py
@@ -1,1 +1,16 @@
-print("hi")
+def solution(x):
+    answer = True
+    lili = []
+    arr = 0
+    z = x
+    while z != 0 :
+        lili.append(z%10)
+        z = z//10
+    for i in range(len(lili)) :
+        arr += lili[i]
+    if x%arr == 0 :
+        answer = True
+    elif x%arr != 0 :
+        answer = False
+    return answer
+print(solution(11))
