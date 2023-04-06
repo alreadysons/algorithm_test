af7cb8804bd071946188a5b0919797a3c9ec4ee9
alreadysons <alreadysons@gmail.com>
2023-04-06 14:15:45 +0900

    list
    https://school.programmers.co.kr/learn/courses/30/lessons/12910?language=python3

--- algorithm.py
+++ algorithm.py
@@ -1,3 +1,9 @@
-def solution(seoul):
-    return "김서방은 {}에 있다".format(seoul.index("Kim"))
-print(solution(["Jane", "Kim"]))    
+def solution(arr, divisor):
+    answer = []
+    for i in range(len(arr)) :
+        if arr[i] % divisor == 0 :
+            answer.append(arr[i])
+    if not answer :
+        return [-1]
+    return sorted(answer)
+print(solution([3,2,6],10))
