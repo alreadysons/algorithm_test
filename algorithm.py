1f26096800887da8be67dd2428f6e3d93dc763d1
alreadysons <alreadysons@gmail.com>
2023-04-06 14:34:02 +0900

    Plus&Minus
    https://school.programmers.co.kr/learn/courses/30/lessons/76501

--- algorithm.py
+++ algorithm.py
@@ -1,2 +1,12 @@
-def solution(phone_number):
-    return '*' * (len(phone_number) - 4) + phone_number[-4:]
+def solution(absolutes, signs):
+    index = []
+    answer = 0
+    for i in range(len(absolutes)) :
+        if signs[i] == True :
+            index.append(absolutes[i])
+        elif signs[i] == False :
+            index.append(-1*absolutes[i])
+    for i in range(len(index)) :
+        answer += index[i]
+    return answer
+print(solution([4,7,12],[True,False,True]))
