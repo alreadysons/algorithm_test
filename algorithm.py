b045a934a91022dc1b1cb02b1ca2f5c753d3c2f3
alreadysons <alreadysons@gmail.com>
2023-04-21 18:41:14 +0900

    protractor
    https://school.programmers.co.kr/learn/courses/30/lessons/120829

--- algorithm.py
+++ algorithm.py
@@ -1,12 +1,12 @@
-def solution(absolutes, signs):
-    index = []
+def solution(angle):
     answer = 0
-    for i in range(len(absolutes)) :
-        if signs[i] == True :
-            index.append(absolutes[i])
-        elif signs[i] == False :
-            index.append(-1*absolutes[i])
-    for i in range(len(index)) :
-        answer += index[i]
+    if angle >0 and angle<90 :
+        answer = 1
+    elif angle == 90 :
+        answer = 2
+    elif angle >90 and angle<180 :
+        answer = 3
+    elif angle == 180 :
+        answer = 4
     return answer
-print(solution([4,7,12],[True,False,True]))
+print(solution(91))
