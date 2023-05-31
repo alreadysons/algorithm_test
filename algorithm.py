fc0a325aaff024b635ae6bf4d4bf168ea0db39d9
alreadysons <alreadysons@gmail.com>
2023-05-31 20:31:23 +0900

    terret
    https://www.acmicpc.net/problem/1002

--- algorithm.py
+++ algorithm.py
@@ -1,19 +1,33 @@
-def solution():
-    N = int(input())
-    S = input()
-    if N<1 and N>100 :
-        return False
-    elif S not in "T" :
-        return False
-    chk_A = 0
-    chk_T = 0
-    for i in range(N) :
-        if S[i] == "T" :
-            chk_T +=1
-        elif S[i] == "A" :
-            chk_A +=1
-    if chk_A>chk_T :
-        return print("A")
-    elif chk_A<chk_T or chk_T == chk_A :
-        return print("T")
-solution()
+import math
+
+def count_possible_positions(x1, y1, r1, x2, y2, r2):
+    # 두 중점 사이의 거리 계산
+    distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    
+    # 두 원이 일치하는 경우 (무한대의 좌표)
+    if distance == 0 and r1 == r2:
+        return -1
+    
+    # 두 원이 외접하거나 내접하는 경우 (한 개의 접점)
+    if distance == r1 + r2 or distance == abs(r1 - r2):
+        return 1
+    
+    # 두 원이 만나는 경우 (두 개의 접점)
+    if abs(r1 - r2) < distance < r1 + r2:
+        return 2
+    
+    # 그 외의 경우 (만날 수 없음)
+    return 0
+
+# 테스트 케이스 개수 입력
+T = int(input())
+
+for _ in range(T):
+    # 테스트 케이스 입력
+    x1, y1, r1, x2, y2, r2 = map(int, input().split())
+    
+    # 류재명이 있을 수 있는 좌표의 수 계산
+    result = count_possible_positions(x1, y1, r1, x2, y2, r2)
+    
+    # 결과 출력
+    print(result)
