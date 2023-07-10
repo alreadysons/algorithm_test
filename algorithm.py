7a82744a6a84e80a320e13beecf2b96a0b3bc59a
alreadysons <alreadysons@gmail.com>
2023-07-10 18:08:23 +0900

    블랙잭
    https://www.acmicpc.net/problem/2798

--- algorithm.py
+++ algorithm.py
@@ -1,12 +1,17 @@
-def find(start,end) :
-    global k
-    if start == end :return #시작과 끝이 같다면 진행x
-    mid = (start+end)//2
-    find(start,mid) ; find(mid+1,end) #배열을 둘로나눠 정렬진행
-    if k <=end-start+1 : #정렬하려는 배열의 크기 >= K인지 확인
-        j = sorted(i[start:end+1]) #원하는 배열만 정렬
-        print(j[k-1]);exit() #A의 몇번째 배열에 들어갔는지 확인 후 정렬된 배열에서 그 수 출력
-    k -= end-start+1 #정렬하려는 배열의 크기 < k 면 우리가 원하는 배열이 아니므로 A에 원소가 들어간 만큼 k를 빼줌
+a,b = map(int,input().split())
+a_list = sorted(map(int,input().split()))
+ab = b
 
-n,k = map(int,input().split()); i = list(map(int,input().split()))
-find(0,n-1);print(-1) #만약 이 과정을 진행하였는데 프로그램을 종료되지 않았다면 저장횟수가 k보다 적은것이니 -1출력
+for i in range(len(a_list)) :
+    chk = 0
+    for j in range(len(a_list)) :
+        for k in range(len(a_list)) :
+            if i==j or j==k or i==k :
+                continue
+            chk = a_list[i]+a_list[j]+a_list[k]
+            if chk > b :
+                continue
+            num = b-chk
+            ab = min(num,ab)
+
+print(b-ab)
