c9bc6ac962ef9fb3ff7f7662b918ff01d9bfffcb
alreadysons <alreadysons@gmail.com>
2023-07-18 17:04:35 +0900

    대표값2
    https://www.acmicpc.net/problem/2587

--- algorithm.py
+++ algorithm.py
@@ -1,4 +1,10 @@
-a,b = map(int,input().split())
-s = list(map(int,input().split()))
-ss = sorted(s)
-print(ss[-b])
+a = []
+for i in range(5) :
+    b = int(input())
+    a.append(b)
+c = sorted(a)
+avg = 0
+for i in range(len(c)) :
+    avg +=c[i]
+print(avg//5)
+print(c[2])
