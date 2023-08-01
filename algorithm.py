acdf08e2c8938bff56715c32ce72c0ed0ff9796c
alreadysons <alreadysons@gmail.com>
2023-08-01 20:50:43 +0900

    백준 스택오류 GPT해결
    입출력 최적화: input() 함수는 입력을 받을 때마다 오버헤드가 크기 때문에, 입력을 한 번에 받아서 처리하는 방법을 사용할 수 있습니다. 예를 들어, sys.stdin을 사용하여 입력을 받는 방법은 더 빠른 입력 처리를 할 수 있습니다.
    
    리스트 대신 덱(deque) 사용: 리스트의 pop() 메서드는 시간 복잡도가 O(1)이지만, 리스트의 맨 앞에서 원소를 제거할 경우 리스트의 모든 원소들을 한 칸씩 앞으로 이동시켜야 하므로 시간 복잡도가 O(n)이 됩니다. 이런 경우 덱(deque)을 사용하여 맨 앞에서도 O(1) 시간에 원소를 제거할 수 있습니다.
    
    결과 문자열 한 번에 출력: 출력을 여러 번 할 경우 많은 시간이 소요될 수 있습니다. 따라서 출력을 미리 문자열로 저장한 후에 한 번에 출력하는 방법을 사용하면 더 효율적입니다.

--- algorithm.py
+++ algorithm.py
@@ -1,22 +1,22 @@
-a=int(input())
-s=[]
-input_com = []
-for i in range(a) :
-    input_com = [x for x in input().split()]
-    if input_com[0] == "push" :
+import sys
+from collections import deque
+
+a = int(input())
+s = deque()
+output = []
+
+for i in range(a):
+    input_com = input().split()
+    if input_com[0] == "push":
         s.append(int(input_com[1]))
-    elif input_com[0] == "top" :
-        if len(s) !=0 :
-            print(s[-1])
-        else : print(-1)    
-    elif input_com[0] == "size" :
-        print(len(s))
-    elif input_com[0] == "pop" :
-        if len(s) !=0 :
-            print(s.pop())
-        else : print(-1)
-    elif input_com[0] == "empty" :
-        if len(s) == 0 :
-            print(1)
-        else :
-            print(0)
+    elif input_com[0] == "top":
+        output.append(str(s[-1]) if s else "-1")
+    elif input_com[0] == "size":
+        output.append(str(len(s)))
+    elif input_com[0] == "pop":
+        output.append(str(s.pop()) if s else "-1")
+    elif input_com[0] == "empty":
+        output.append("0" if s else "1")
+
+# 결과 문자열을 한 번에 출력
+sys.stdout.write("\n".join(output))
