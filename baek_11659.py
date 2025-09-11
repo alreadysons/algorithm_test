b963524ea6433b38896d17e8d031f9b8b00e9d2f
alreadysons <alreadysons@gmail.com>
2025-09-11 09:07:07 +0900

    누적합(prefix sum)으로 개선

--- baek_11659.py
+++ baek_11659.py
@@ -23,11 +23,10 @@
 import sys
 input = sys.stdin.readline
 n,m = map(int,input().split())
-s = list(map(int,input().split()))
-num = 0
+numbers = list(map(int,input().split()))
+prefix = [0] * (n+1)
+for i in range(1,n+1) :
+    prefix[i] = prefix[i-1] + numbers[i-1]
 for _ in range(m) :
     i,j = map(int,input().split())
-    for k in range(i-1,j) :
-        num += s[k]
-    print(num)
-    num = 0
+    print(prefix[j] - prefix[i-1])
